eef4856e08c4bf08eb586d0c1ed980f8cc64c8bb
Christopher Danan <chrisddan@gmail.com>
2015-02-28 16:39:50 -0800

    Initial commit: *Create index.html and fizzbuzz.js, and link the two together.

---
+++ Javascripts/fizzbuzz.js
@@ -0,0 +1,16 @@
+/*Name: Christopher Dancarlo Danan
+ *Created: February 28, 2015
+ *Modified: February 28, 2015
+ *Purpose: Solve the FizzBuzz problem.
+ 		   From the book: 	Write a program that prints the numbers from 1 to 100. 
+ 		   					But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz.” 
+ 		   					For numbers which are multiples of both three and five print “FizzBuzz.” 
+*/
+
+var main = function(){
+	"use strict";
+
+	console.log("Hello Vane!");
+}
+
+$(document).ready(main);

f7b271aaa42ee6a284bd230f6d5d83a4114f91fb
Christopher Danan <chrisddan@gmail.com>
2015-02-28 18:46:45 -0800

    Solve FizzBuzz problem. Solution only outputs to console.

--- Javascripts/fizzbuzz.js
+++ Javascripts/fizzbuzz.js
@@ -7,10 +7,30 @@
  		   					For numbers which are multiples of both three and five print “FizzBuzz.” 
 */
 
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+
 var main = function(){
 	"use strict";
 
-	console.log("Hello Vane!");
-}
+	var i = 1, //Loop counter.
+		stringArr = []; //Holds string of numbers, "Fizz", "Buzz", and "FizzBuzz".
+
+	for(i; i <= 100; i++){
+		if((i % 3 === 0) && (i % 5 !== 0)){
+			stringArr.push("Fizz");
+		} else if((i % 3 !== 0) && (i % 5 === 0)){
+			stringArr.push("Buzz");
+		} else if((i % 3 === 0) && (i % 5 === 0)){
+			stringArr.push("FizzBuzz");
+		} else{
+			stringArr.push(i);
+		}
+	}//End for-loop.
+
+	stringArr.forEach(function(element){
+		console.log(element);
+	});
+};
 
 $(document).ready(main);
